5da0d97c1fa5e8718a20605223d1e8cbb506c6fc
Indunil1992 <indunil@adroitlogic.com>
2020-02-27 16:27:27 +0530

    sqs 1 R

--- lambda.js
+++ lambda.js
@@ -1,4 +1,23 @@
+let AWS = require('aws-sdk');
+const sqs = new AWS.SQS();
+
 exports.handler = async (event) => {
-    
-    return {"message": "Successfully executed"};
+    try {
+        let data = await sqs.receiveMessage({
+            QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/indunil-message-queue`,
+            MaxNumberOfMessages: 1,
+            VisibilityTimeout: 30,
+            WaitTimeSeconds: 0,
+            AttributeNames: ['All']
+        }).promise();
+        console.log("data");
+                console.log(data);
+    } catch (err) {
+        // error handling goes here
+                console.log("err");
+                console.log(err);
+
+    };
+
+    return { "message": "Successfully executed" };
 };
