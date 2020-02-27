1d7f713e13e3d8005b30b080c1951cefe563a447
Indunil1992 <indunil@adroitlogic.com>
2020-02-27 17:35:40 +0530

    s3

--- lambdasqs.js
+++ lambdasqs.js
@@ -1,20 +1,16 @@
 let AWS = require('aws-sdk');
-const sqs = new AWS.SQS();
+const s3 = new AWS.S3();
 
 exports.handler = async (event) => {
     try {
-        let data = await sqs.receiveMessage({
-            QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/indunil.fifo`,
-            MaxNumberOfMessages: 1,
-            VisibilityTimeout: 30,
-            WaitTimeSeconds: 0,
-            AttributeNames: ['SequenceNumber']
+        let data = await s3.listObjects({
+            Bucket: "as2-test-lahiru",
+            MaxKeys: 10
         }).promise();
 
     } catch (err) {
         // error handling goes here
     };
-
 
     return { "message": "Successfully executed" };
 };
