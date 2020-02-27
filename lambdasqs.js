c55fee9cbb3ae20fe924cc95747aa659bf65b1cc
Indunil1992 <indunil@adroitlogic.com>
2020-02-27 18:01:39 +0530

    dew

--- lambdasqs.js
+++ lambdasqs.js
@@ -1,15 +1,10 @@
 let AWS = require('aws-sdk');
-const ddb = new AWS.DynamoDB.DocumentClient();
+const sns = new AWS.SNS();
 
 exports.handler = async (event) => {
-
     try {
-        let data = await ddb.get({
-            TableName: "hirutest",
-            Key: {
-                price: "1",
-                colour: "2"
-            }
+        let data = await sns.unsubscribe({
+            SubscriptionArn: "arn:aws:sns:us-east-1:318300609668:TestSNS:bec16985-353e-42f0-8e40-952321c5e4fe"
         }).promise();
 
     } catch (err) {
