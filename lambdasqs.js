24b35505416a4a116f9d5b6b0381400640afb046
Indunil1992 <indunil@adroitlogic.com>
2020-02-27 17:53:30 +0530

    cx

--- lambdasqs.js
+++ lambdasqs.js
@@ -1,18 +1,20 @@
 let AWS = require('aws-sdk');
-const sqs = new AWS.SQS();
+const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
+
     try {
-        let data = await sqs.sendMessage({
-            QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/indunil-message-queue`,
-            MessageBody: "cxg",
-            DelaySeconds: 0
+        let data = await ddb.get({
+            TableName: "hirutest",
+            Key: {
+                price: "1",
+                colour: "2"
+            }
         }).promise();
 
     } catch (err) {
         // error handling goes here
     };
-
 
     return { "message": "Successfully executed" };
 };
